c7cd4de6297d08384ce6fbb6e954629546460fcf
nathan <yuting870116@gmail.com>
2022-11-08 01:09:51 +0800

    import tailwind.css

---
+++ src/components/index/home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <section className="w-screen h-screen bg-secondary">
+    <div className="flex justify-center items-center">123</div>
+  </section>
+);
