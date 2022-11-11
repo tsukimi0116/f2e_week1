01c5f91c24317f8c4745a0d0f8907b3578305794
nathan <yuting870116@gmail.com>
2022-11-11 18:12:37 +0800

    feat: nav-close按鈕完成

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -6,23 +6,27 @@ import IcMenuStrategy from '@/ic/ic_menu_strategy.png';
 import IcMenuJob from '@/ic/ic_menu_job.png';
 
 export default () => (
-  <section className="fixed h-screen bg-nav-bg w-40 bg-right bg-no-repeat bg-cover text-navFont flex-col items-center text-center">
-    <div className="my-10">
-      <Image className="m-auto" src={IcMenuInfo} alt="" width={60} />
-      關卡資訊
-    </div>
-    <div className="mt-10">
-      <Image className="m-auto" src={IcMenuList} alt="" width={60} />
-      作品列表
-    </div>
-    <div className="mt-10">
-      <Image className="m-auto" src={IcMenuStrategy} alt="" width={60} />
-      攻略資源
-    </div>
-    <div className="mt-10">
-      <Image className="m-auto" src={IcMenuJob} alt="" width={60} />
-      求職專區
-    </div>
-    <div className="bg-nav-close-btn w-16 inline-block" />
-  </section>
+  <div className="fixed flex items-center">
+    <section className="h-screen bg-navBg w-32 bg-no-repeat text-navFont flex-col items-center">
+      <div className="my-10 ml-12">
+        <Image className="" src={IcMenuInfo} alt="" width={60} />
+        關卡資訊
+      </div>
+      <div className="mt-10 ml-12">
+        <Image className="" src={IcMenuList} alt="" width={60} />
+        作品列表
+      </div>
+      <div className="mt-10 ml-12">
+        <Image className="" src={IcMenuStrategy} alt="" width={60} />
+        攻略資源
+      </div>
+      <div className="mt-10 ml-12">
+        <Image className="" src={IcMenuJob} alt="" width={60} />
+        求職專區
+      </div>
+      <div className="bg-nav-close-btn w-16 inline-block" />
+    </section>
+    <div className="w-5 h-screen bg-nav-bg bg-cover bg-right" />
+    <div className="w-16 h-16 bg-nav-close-btn bg-cover -ml-px" />
+  </div>
 );
