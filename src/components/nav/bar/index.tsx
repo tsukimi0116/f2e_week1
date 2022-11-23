e12aa3e082daad818ac1ede9984820e492525886
t_nathan <nathan.hyt@link8tech.tw>
2022-11-23 14:42:53 +0800

    feat: nav切板完成

--- src/components/nav/bar/index.tsx
+++ src/components/nav/bar/index.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 
 export default () => (
-  <>
+  <nav className="fixed flex items-center z-50">
     <input id="menu-control" className="peer hidden" type="checkbox" />
-    <nav className="fixed -left-32 flex items-center z-50 transition-all duration-500 peer-checked:translate-x-32">
-      <ul className="h-screen bg-navBg w-32 bg-no-repeat text-navFont text-center flex-col items-center ">
-        <li className="my-10 ml-6 pt-16 bg-menu-info bg-menu-size bg-no-repeat bg-top">
-          關卡資訊
-        </li>
-        <li className="mt-10 ml-6 pt-16 bg-menu-list bg-menu-size bg-no-repeat bg-top">
-          作品列表
-        </li>
-        <li className="mt-10 ml-6 pt-16 bg-menu-strategy bg-menu-size bg-no-repeat bg-top">
-          攻略資源
-        </li>
-        <li className="mt-10 ml-6 pt-16 bg-menu-job bg-menu-size bg-no-repeat bg-top">
-          求職專區
-        </li>
-        <div className="bg-nav-close-btn w-16 inline-block" />
-      </ul>
-      <article className="w-5 h-screen bg-nav-bg bg-cover bg-right" />
-      <label htmlFor="menu-control" className="w-16 h-16 bg-nav-open-btn hover:bg-nav-open-btn-h bg-cover -ml-px" />
-    </nav>
-  </>
+    <ul className="relative h-screen bg-navBg w-32 -left-32  bg-no-repeat text-navFont text-center flex-col items-center transition-all duration-300 peer-checked:translate-x-32">
+      <li className="my-10 ml-6 pt-16 bg-menu-info bg-menu-size bg-no-repeat bg-top hover:bg-menu-info-h">
+        關卡資訊
+      </li>
+      <li className="mt-10 ml-6 pt-16 bg-menu-list bg-menu-size bg-no-repeat bg-top hover:bg-menu-list-h">
+        作品列表
+      </li>
+      <li className="mt-10 ml-6 pt-16 bg-menu-strategy bg-menu-size bg-no-repeat bg-top hover:bg-menu-strategy-h">
+        攻略資源
+      </li>
+      <li className="mt-10 ml-6 pt-16 bg-menu-job bg-menu-size bg-no-repeat bg-top hover:bg-menu-job-h">
+        求職專區
+      </li>
+      <div className="bg-nav-close-btn w-16 inline-block" />
+    </ul>
+    <article className="relative -left-32 w-5 h-screen bg-nav-bg bg-cover bg-right transition-all duration-300 peer-checked:translate-x-32" />
+    <label htmlFor="menu-control" className="relative w-16 h-16 -ml-px bg-nav-open-btn bg-cover peer-checked:bg-nav-close-btn -left-32 transition-all duration-300 hover:bg-nav-open-btn-h peer-checked:translate-x-32 peer-checked:hover:bg-nav-close-btn-h" />
+  </nav>
 );
--- src/components/nav/info/index.tsx
+++ src/components/nav/info/index.tsx
@@ -4,12 +4,12 @@ import BtnJoinHand from '@/btn/btn_joinHand.gif';
 
 export default () => (
   <>
-    <article className="w-20 h-20 z-50 fixed top-6 right-6 bg-btn-user bg-no-repeat bg-contain hover:bg-btn-user-h" />
-    <article className="w-64 h-40 fixed bottom-8 left-10 z-50 bg-map-bg" />
-    <article className="w-14 h-30 fixed bottom-20 right-11 z-50 text-primary font-bold text-center text-2xl">
+    <article className="w-20 h-20 z-40 fixed top-6 right-6 bg-btn-user bg-no-repeat bg-contain hover:bg-btn-user-h" />
+    <article className="w-64 h-40 fixed bottom-8 left-10 z-40 bg-map-bg" />
+    <article className="w-14 h-30 fixed bottom-20 right-11 z-40 text-primary font-bold text-center text-2xl">
       JOIN
       <Image src={BtnJoinHand} alt="" className="" />
     </article>
-    <article className="w-24 h-12 fixed bottom-4 right-4 z-50 bg-btn-join bg-no-repeat bg-contain hover:bg-btn-join-h" />
+    <article className="w-24 h-12 fixed bottom-4 right-4 z-40 bg-btn-join bg-no-repeat bg-contain hover:bg-btn-join-h" />
   </>
 );
